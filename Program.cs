c2f498d8847d4127bc301491cf520742d28abd8b
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-01 21:58:43 +0300

    Написал заполнение массива случайными неповторяющимися числами

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int[] perechislenie=new int[1000000];
+int i=0;
+int v=perechislenie.Length;
+while (i<v)
+{
+    perechislenie[i] = i; //заполнили массив числами от 1 до миллиона
+    i++;
+}
+//Console.WriteLine(perechislenie[899999]);
+// Заполняем второй массив на 100 000 элементов случайными числами
+int[] arrayData=new int[100000];
+int v1=arrayData.Length;
+i=0;
+int temp=0;
+while (i<v1)
+{
+    temp=new Random().Next(0,v);
+    if (perechislenie[temp]>0)
+    {
+        arrayData[i]=temp;
+        perechislenie[temp]=-1;
+    }
+    i++;
+}
+i=100;
+while (i<120)
+{
+    Console.WriteLine($" , {arrayData[i]}, {i}");
+    i++;
+}
