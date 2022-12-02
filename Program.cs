ddca6cb4bca217aa356c8d4d04ba87524b34406d
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-02 12:12:42 +0300

    трудности с алгоритмом бинарного кода

--- Program.cs
+++ Program.cs
@@ -1,49 +1,49 @@
 ﻿Console.Clear();
-int[] perechislenie=new int[100];
-int i=0;
-int v=perechislenie.Length; // v - длина массива - источника чисел
-while (i<v)
+int[] perechislenie = new int[100];
+int i = 0;
+int v = perechislenie.Length; // v - длина массива - источника чисел
+while (i < v)
 {
     perechislenie[i] = i; //заполнили массив числами от 1 до миллиона
     i++;
 }
 //Console.WriteLine(perechislenie[899999]);
 // Заполняем второй массив на 100 000 элементов случайными числами
-int[] arrayData=new int[10];
-int v1=arrayData.Length; // Длина массива данных
-i=0;
-int temp=0;
-while (i<v1)
+int[] arrayData = new int[10];
+int v1 = arrayData.Length; // Длина массива данных
+i = 0;
+int temp = 0;
+while (i < v1)
 {
-    temp=new Random().Next(1,v);
-    if (perechislenie[temp]>0)
+    temp = new Random().Next(1, v);
+    if (perechislenie[temp] > 0)
     {
-        arrayData[i]=temp;
-        perechislenie[temp]=-1;
+        arrayData[i] = perechislenie[temp];
+        perechislenie[temp] = -1;
     }
     i++;
 }
 Console.WriteLine("__________________");
 Console.WriteLine("__________________");
-i=0;
-while (i<10)
+i = 0;
+while (i < 10)
 {
     Console.WriteLine($"{arrayData[i]}, {i}");
     i++;
 }
 
 //Сортируем пузырьком заполненный массив
-i=0;
-while (i<v1)
+i = 0;
+while (i < v1)
 {
-    int j=0;
-    while (j<v1-1)
+    int j = 0;
+    while (j < v1 - 1)
     {
-        if (arrayData[j]>arrayData[j+1])
+        if (arrayData[j] > arrayData[j + 1])
         {
-            temp=arrayData[j];
-            arrayData[j]=arrayData[j+1];
-            arrayData[j+1]=temp;
+            temp = arrayData[j];
+            arrayData[j] = arrayData[j + 1];
+            arrayData[j + 1] = temp;
         }
         j++;
     }
@@ -59,17 +59,30 @@ Console.Write($"В выбранной ячейке массива число {ar
 Console.WriteLine("__________________________________________________________");
 Console.Write("Введите искомое значение в массиве: ");
 int search = int.Parse(Console.ReadLine());
-
-if (arrayData[v1/2]>search)
-
-
-
-
-
-Console.WriteLine("__________________");
-i=0;
-while (i<10)
+int polovina = v1/2;
+while (search != arrayData[polovina])
 {
-    Console.WriteLine($"{arrayData[i]}, {i}");
-    i++;
+    if (search > arrayData[polovina])
+    {
+        polovina = polovina + polovina / 2;
+    }
+    else
+    {
+        if (search < arrayData[polovina])
+        {
+            polovina = polovina - polovina / 2;
+        }
+    }
 }
+Console.WriteLine($"Число {search} находится в ячейке номер {polovina}");
+
+
+
+
+//Console.WriteLine("__________________");
+//i = 0;
+//while (i < 10)
+//{
+//    Console.WriteLine($"{arrayData[i]}, {i}");
+//    i++;
+//}
