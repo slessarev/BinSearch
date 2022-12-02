3448bdff1c764056b1a65a09f557aa1faf7eed76
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-02 15:15:34 +0300

    Исправил ошибку заполнения. В массиве оставались незаполненные ячейки и в них хранился 0

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.Clear();
-int[] perechislenie = new int[1000000];
+Console.WriteLine("Чтобы выйти - введите отрицательное число");
+int[] perechislenie = new int[100000];
 int i = 0;
 int v = perechislenie.Length; // v - длина массива - источника чисел
 while (i < v)
@@ -9,7 +10,7 @@ while (i < v)
 }
 //Console.WriteLine(perechislenie[899999]);
 // Заполняем второй массив на 100 000 элементов случайными числами
-int[] arrayData = new int[100000];
+int[] arrayData = new int[10000];
 int v1 = arrayData.Length; // Длина массива данных
 i = 0;
 int temp = 0;
@@ -20,16 +21,16 @@ while (i < v1)
     {
         arrayData[i] = perechislenie[temp];
         perechislenie[temp] = -1;
+        i++;
     }
-    i++;
 }
 //Console.WriteLine("__________________");
 //Console.WriteLine("__________________");
 //i = 0;
 //while (i < 10)
 //{
- //   Console.WriteLine($"{arrayData[i]}, {i}");
- //   i++;
+//   Console.WriteLine($"{arrayData[i]}, {i}");
+//   i++;
 //}
 
 //Сортируем пузырьком заполненный массив
@@ -51,43 +52,46 @@ while (i < v1)
 }
 
 //Создаю алгоритм бинарного поиска
-Console.WriteLine("__________________________________________________________");
-Console.Write("Введите ячейку массива чтобы посмотреть какое там число: ");
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine($"В выбранной ячейке массива число {arrayData[num]}.");
 
-Console.WriteLine("__________________________________________________________");
-Console.Write("Введите искомое значение в массиве: ");
-int search = int.Parse(Console.ReadLine());
-//int polovina = v1 / 2;
-//while (search != arrayData[polovina])
-//{
-//if (search > arrayData[polovina])
-//{
-//polovina = polovina + polovina / 2;
-//}
-//else
-//{
-//        polovina = polovina - polovina / 2;
-//}
-//}
-
-int low = 0;
-int high = v1 - 1;
-while (low <= high)
+int search = 0;
+while (search >= -0)
 {
-    int mid = (low + high)/2;
-    int guess = arrayData[mid];
-    if (guess == search)
-    {
-        Console.WriteLine($"Число {search} находится в ячейке номер {mid}");
-    }
-    if (guess > search) high = mid - 1;
-    else low = mid + 1;
-//    Console.WriteLine($"Значение не существует");
-}
-//Console.WriteLine($"Число {search} находится в ячейке номер {polovina}");
+    Console.WriteLine("__________________________________________________________");
+    Console.Write("Введите ячейку массива чтобы посмотреть какое там число: ");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine($"В выбранной ячейке массива число {arrayData[num]}.");
+    Console.WriteLine("__________________________________________________________");
+    Console.Write("Введите искомое значение в массиве: ");
+    search = int.Parse(Console.ReadLine());
+    //int polovina = v1 / 2;
+    //while (search != arrayData[polovina])
+    //{
+    //if (search > arrayData[polovina])
+    //{
+    //polovina = polovina + polovina / 2;
+    //}
+    //else
+    //{
+    //        polovina = polovina - polovina / 2;
+    //}
+    //}
 
+    int low = 0;
+    int high = v1 - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int guess = arrayData[mid];
+        if (guess == search)
+        {
+            Console.WriteLine($"Число {search} находится в ячейке номер {mid}");
+        }
+        if (guess > search) high = mid - 1;
+        else low = mid + 1;
+        //    Console.WriteLine($"Значение не существует");
+    }
+    //Console.WriteLine($"Число {search} находится в ячейке номер {polovina}");
+}
 
 
 
